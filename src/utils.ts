74761350a5b94a5707edf1eaed2bc7a18cf6657e
jinzhongjia <mail@nvimer.org>
2024-01-19 18:32:34 +0800

    add some function

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,5 @@
-import { CString, ptr } from "bun:ffi";
+import { CString, ptr, type Pointer } from "bun:ffi";
+
 /**
  * Convert a String to C-String.
  * @param {string} value
@@ -8,4 +9,13 @@ export function toCString(value: string): CString {
   const u8_arr = new TextEncoder().encode(value + "\0");
   const str_ptr = ptr(u8_arr, 0);
   return new CString(str_ptr, 0, u8_arr.byteLength);
+}
+
+/**
+ * convert array to a pointer.
+ * @param {ArrayBuffer} array
+ * @returns pointer
+ */
+export function arrayToPtr(array: ArrayBuffer) {
+  return ptr(array, 0);
 }
--- src/webui.ts
+++ src/webui.ts
@@ -1,5 +1,5 @@
-import { webui, BindCallback, Browsers } from "./types";
-import { toCString } from "./utils";
+import { webui, BindCallback, SetFileHandlerCallbak, Browsers } from "./types";
+import { arrayToPtr, toCString } from "./utils";
 
 const WEBUI_MAX_IDS = 256;
 
@@ -205,4 +205,177 @@ export class WebUI {
     const tmp_path = toCString(path);
     return webui.webui_set_default_root_folder(tmp_path.ptr);
   }
+
+  /**
+   *
+   * Close all open windows. `wait()` will return (Break).
+   * @param SetFileHandlerCallbak callback for the file handler
+   * @example
+   * ```ts
+   * myWindow.setFileHandler(callback);
+   * ```
+   */
+  setFileHandler(callback: SetFileHandlerCallbak) {
+    webui.webui_set_file_handler(this.handle, callback.ptr);
+  }
+
+  /**
+   *
+   * Check if the window is still running.
+   * @return Returns True if the window is running.
+   * @example
+   * ```ts
+   * const val = myWindow.isShown();
+   * ```
+   */
+  isShown(): boolean {
+    return webui.webui_is_shown(this.handle);
+  }
+
+  /**
+   *
+   * Set the maximum time in seconds to wait for the browser to start.
+   * @example
+   * ```ts
+   * WebUI.setTimeout(5);
+   * ```
+   */
+  static setTimeout(second: number) {
+    webui.webui_set_timeout(second);
+  }
+
+  /**
+   *
+   * Set the default embedded HTML favicon.
+   * @param icon The icon as string: `<svg>...</svg>`
+   * @param icon_type The icon type: `image/svg+xml`
+   * @example
+   * ```ts
+   * myWindow.setIcon("<svg>...</svg>", "image/svg+xml");
+   * ```
+   */
+  setIcon(icon: string, icon_type: string) {
+    // TODO: this should support file u8array
+    const tmp_icon = toCString(icon);
+    const tmp_icon_type = toCString(icon_type);
+
+    webui.webui_set_icon(this.handle, tmp_icon.ptr, tmp_icon_type.ptr);
+  }
+
+  /**
+   *
+   * Base64 encoding. Use this to safely send text based data to the UI.
+   *
+   * @param string The string to encode
+   * @returns string
+   *
+   * @example
+   * ```ts
+   * WebUI.encode("Hello");
+   * ```
+   *
+   */
+  static encode(val: string) {
+    const tmp_val = toCString(val);
+    const res = webui.webui_encode(tmp_val.ptr);
+    if (res.ptr != 0) {
+      return res.toString();
+    }
+    // TODO: res.ptr maybe null
+  }
+
+  /**
+   *
+   * Base64 decoding. Use this to safely decode received Base64 text from the UI.
+   *
+   * @param string The string to decode
+   * @returns string
+   *
+   * @example
+   * ```ts
+   * WebUI.decode("SGVsbG8=");
+   * ```
+   *
+   */
+  static decode(val: string) {
+    const tmp_val = toCString(val);
+    const res = webui.webui_decode(tmp_val.ptr);
+    if (res.ptr != 0) {
+      return res.toString();
+    }
+    // TODO: res.ptr maybe null
+  }
+
+  // TODO: whether we should adapt free and malloc?
+
+  /**
+   *
+   * Safely send raw data to the UI.
+   * @param string the function name
+   * @param raw The raw data buffer
+   * @example
+   * ```ts
+   * myWindow.sendRaw("myJavascriptFunction", myBuffer);
+   * ```
+   */
+  sendRaw(functionName: string, raw: ArrayBuffer) {
+    const tmp_functionName = toCString(functionName);
+    const ptr = arrayToPtr(raw);
+    webui.webui_send_raw(
+      this.handle,
+      tmp_functionName.ptr,
+      ptr,
+      raw.byteLength,
+    );
+  }
+
+  /**
+   *
+   * Set a window in hidden mode. Should be called before `show()`.
+   * @param status - True to hide, false to show.
+   * @example
+   * ```ts
+   * myWindow.setHide(true);
+   * ```
+   */
+  setHide(status: boolean) {
+    webui.webui_set_hide(this.handle, status);
+  }
+
+  /**
+   *
+   * Set the window size.
+   * @param width - The width of the window.
+   * @param height - The height of the window.
+   * @example
+   * ```ts
+   * myWindow.setSize(800, 600);
+   * ```
+   */
+  setSize(width: number, height: number) {
+    if (width >= 0 && height >= 0) {
+      webui.webui_set_size(this.handle, width, height);
+    } else {
+      // TODO: maybe should throw an error
+    }
+  }
+
+  /**
+   * Set the window position.
+   *
+   * @param x - The x-coordinate of the window.
+   * @param y - The y-coordinate of the window.
+   * ```ts
+   * myWindow.setPosition(100, 100);
+   * ```
+   */
+  setPosition(x: number, y: number) {
+    if (x >= 0 && y >= 0) {
+      webui.webui_set_position(this.handle, x, y);
+    } else {
+      // TODO: maybe should throw an error
+    }
+  }
+
+  setProfile() {}
 }
